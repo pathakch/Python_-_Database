ad8be029fb3fea89fd1bbd15daaa1f633d6d4936
ckp <ckp4392@gmail.com>
2023-03-04 15:48:59 +0530

    new_changes

--- new_practice.py
+++ new_practice.py
@@ -1,1 +1,15 @@
 import sqlite3
+
+record = (123,'bob',34444)
+records = [(1400,'Max',150000),(14001,'Jennier',250000)] 
+try:
+    conn=sqlite3.connect("new_db.db")
+    cur=conn.cursor()
+    #cur.execute("create table table_new(rollno integer,name text,feesn real)"
+
+    cur.execute("insert into table_new values(1,'anil',43000)")
+    conn.commit()
+    conn.close()
+
+except Exception as err:
+    print(err)
