01e1083876ee9284f5224bd1225ca685dc57c9e9
ckp <ckp4392@gmail.com>
2023-03-05 11:19:32 +0530

    docstring added in pycharm and visual studio code files

--- mysql_connection_vs.py
+++ mysql_connection_vs.py
@@ -1,5 +1,12 @@
 import mysql.connector
 
+""" This file is for visual studio environment testing
+    This code is working fine in visual studio code 
+    This mySQL connection is for localhost means 
+    this server is running on this same laptop in mySQL database
+    And it's found to be working.
+"""
+
 conn = mysql.connector.connect(host='localhost',password='mqb237@007H',user='root')
 if conn.is_connected():
     print("connection established")
--- sql_connection.py
+++ sql_connection.py
@@ -1,5 +1,11 @@
 import mysql.connector
 
+"""This file is for pycharm testing. 
+   This code has been tested that its working in pycharm or not
+   This mySQL server is localhost server means it's running on this same
+   laptop in mySQL database and found to be working
+"""
+
 conn = mysql.connector.connect(host='localhost',password='mqb237@007H',user='root')
 if conn.is_connected():
     print("connection established")
